0e34d60a791c9a83443b5428688db40b99cbfc0b
favtuts <favtuts@gmail.com>
2023-02-03 22:54:10 +0700

    day_02 use external HTML file for component content

--- day_02/Angular8Demo/src/app/app.component.ts
+++ day_02/Angular8Demo/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<h1>Component is the main Building Blockin Angular</h1> <h2>Angular 8 Samples</h2>',
-  styleUrls: ['./custom.css']
-})
-export class AppComponent{
-
-}
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls : ['./custom.css']  
+})  
+export class AppComponent {  
+    
+} 

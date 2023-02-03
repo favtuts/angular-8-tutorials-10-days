c7b45362d18eeb055c1ad4aa3af5a0c4840194f8
favtuts <favtuts@gmail.com>
2023-02-03 09:54:40 +0700

    develop first component with inline HTML contains

--- day_02/Angular8Demo/src/app/app.component.ts
+++ day_02/Angular8Demo/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',  
+  template:'<h1>Component is the main Building Block in Angular</h1>'
 })
 export class AppComponent {
-  title = 'Welcome to Angular 8 Learning Series...';
+    
 }

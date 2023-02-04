ead51654e4843ae4cd05011eb358a8f6662e8c7c
favtuts <favtuts@gmail.com>
2023-02-04 16:29:56 +0700

    day_3 implement Interpolation for different data types

--- day_03/Angular8Demo/src/app/app.component.ts
+++ day_03/Angular8Demo/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';  
-  
-@Component({  
-  selector: 'app-root',  
-  templateUrl: './app.component.html',  
-  styleUrls : ['./custom.css']  
-})  
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
-  title = 'Welcome to Angular 8 Learning Series...';
+  public value1: number = 10;
+  public array1: Array<number> = [10, 22, 14];
+  public dt1: Date = new Date();
+
+  public status: Boolean = true;
+
+  public returnString(): string {
+    return "String return from function";
+  }
 }

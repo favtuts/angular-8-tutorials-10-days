62dcbb1c8252d7046b8ef409fec3d21a91bac6e5
favtuts <favtuts@gmail.com>
2023-02-04 16:51:37 +0700

    day_2 demostrate how to implement event-based binding

--- day_03/Angular8Demo/src/app/app.component.ts
+++ day_03/Angular8Demo/src/app/app.component.ts
@@ -3,16 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./custom.css']
 })
 export class AppComponent {
-  public value1: number = 10;
-  public array1: Array<number> = [10, 22, 14];
-  public dt1: Date = new Date();
 
-  public status: Boolean = true;
-
-  public returnString(): string {
-    return "String return from function";
+  public showAlert() : void {
+    console.log('You clicked on the button...');
+    alert("Click Event Fired...");
   }
 }

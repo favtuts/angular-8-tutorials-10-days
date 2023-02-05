e3e9512fc1a7bc7c765cb83a3f9a0c01a7316d48
favtuts <favtuts@gmail.com>
2023-02-05 15:25:25 +0700

    day_4 demostrate how to create a custom attribute directive to change element color

--- day_04/Angular8Demo/src/app/app.module.ts
+++ day_04/Angular8Demo/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';  
+import { FormsModule } from '@angular/forms';  
+  
+import { AppComponent } from './app.component';  
+import { ColorChangeDirective } from './app.directive';  
+  
+@NgModule({  
+  declarations: [  
+    AppComponent,ColorChangeDirective  
+  ],  
+  imports: [  
+    BrowserModule,FormsModule  
+  ],  
+  providers: [],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  

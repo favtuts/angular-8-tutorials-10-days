8f7ab34d94e227d9d4bd65ce25b030249fdab19e
favtuts <favtuts@gmail.com>
2023-02-05 16:06:51 +0700

    day_5 init the first source

---
+++ day_05/Angular8Demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls : ['./custom.css']  
+})  
+export class AppComponent {
+  title = 'Welcome to Angular 8 Learning Series...';
+}
